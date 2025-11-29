7ce130e031bb4f28fab5e7756fa9427b3eed0e38
AckroydAI <36539461+AImissq@users.noreply.github.com>
2025-11-29 04:50:23 +0000

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const mongoose=require('mongoose');
 const app = express();
 
 
-mongoose.connect("mongodb+srv://sachin:DulMTiGgcyqtugWl@cluster0.7yzpdlf.mongodb.net/&w=majority&appName=Cluster0")
+mongoose.connect('mongodb://127.0.0.1:27017/gallerydb')
 .then(()=>{
   console.log("Connected to database");
 }).catch((error)=>{
